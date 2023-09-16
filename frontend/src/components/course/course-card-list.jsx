9b5075e02110101d73e043a93a2503383a1fae5d
benoitthefirst <41606173+benoitthefirst@users.noreply.github.com>
2023-09-16 13:59:44 +0200

    set to only show tests for that school

--- frontend/src/components/course/course-card-list.jsx
+++ frontend/src/components/course/course-card-list.jsx
@@ -2,9 +2,11 @@ import CourseCard from "/src/components/course/course-card";
 import Info from "/src/components/shared/info";
 
 export default function CourseCardList({ tests, isTest = true }) {
+
+  const user = JSON.parse(localStorage.getItem("user"));
   return tests.length > 0 ? (
     <div className="grid gap-4 lg:grid-cols-2 lg:gap-8 lg:px-8 xl:grid-cols-3">
-      {tests.map((test) => (
+      {tests?.filter(x => x.school_name == user?.school_name).map((test) => (
         <CourseCard test={test} key={test.testId} isTest={isTest} />
       ))}
     </div>
--- frontend/src/pages/Assignments/components/Assignment-Card-List.jsx
+++ frontend/src/pages/Assignments/components/Assignment-Card-List.jsx
@@ -2,9 +2,11 @@ import AssignmentCard from "./Assignment-Card";
 import Info from "/src/components/shared/info";
 
 export default function AssignmentCardList({ assignments }) {
+  console.log("Data: ",assignments)
+  const user = JSON.parse(localStorage.getItem("user"));
   return assignments.length > 0 ? (
     <div className="grid gap-4 lg:grid-cols-2 lg:gap-8 lg:px-8 xl:grid-cols-3">
-      {assignments.map((assignment) => (
+      {assignments?.filter(x => x.school_name == user?.school_name).map((assignment) => (
         <AssignmentCard assignment={assignment} key={assignments.assignmentId} />
       ))}
     </div>
